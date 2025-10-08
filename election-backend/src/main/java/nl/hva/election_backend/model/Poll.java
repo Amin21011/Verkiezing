ba1742aa31998499c30fea0f976cf7a8fcec112a
Amin Mousoun <amin.mousoun@hva.nl>
2025-10-08 19:40:39 +0200

    feat: adding the poll model

---
+++ election-backend/src/main/java/nl/hva/election_backend/model/Poll.java
@@ -0,0 +1,39 @@
+package nl.hva.election_backend.model;
+
+import java.util.List;
+
+public class Poll {
+    private String question;
+    private List<String> options;
+    private int[] votes;
+
+    public Poll(String question, List<String> options) {
+        this.question = question;
+        this.options = options;
+        this.votes = new int[options.size()];
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public int[] getVotes() {
+        return votes;
+    }
+
+    public void vote(int optionIndex) {
+        if (optionIndex >= 0 && optionIndex < votes.length) {
+            votes[optionIndex]++;
+        }
+    }
+
+    public void resetVote(int optionIndex) {
+        if (optionIndex >= 0 && optionIndex < votes.length && votes[optionIndex] > 0) {
+            votes[optionIndex]--;
+        }
+    }
+}
