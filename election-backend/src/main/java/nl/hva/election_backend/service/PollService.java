377b212c8ad5165cdfbe179104c46472fd2e54e2
Amin Mousoun <amin.mousoun@hva.nl>
2025-10-08 19:56:09 +0200

    feat: adding comments aan de code

--- election-backend/src/main/java/nl/hva/election_backend/service/PollService.java
+++ election-backend/src/main/java/nl/hva/election_backend/service/PollService.java
@@ -7,14 +7,17 @@ import java.util.List;
 public class PollService {
     private List<Poll> polls = new ArrayList<>();
 
+    // Toevoeging van een nieuwe poll aan de lijst
     public void addPoll(Poll poll) {
         polls.add(poll);
     }
 
+    // Het teruggeven van de lijst met alle polls
     public List<Poll> getPolls() {
         return polls;
     }
 
+    // Het zoeken van een poll op basis van de index in de lijst
     public Poll getPollByIndex(int index) {
         if (index >= 0 && index < polls.size()) {
             return polls.get(index);
@@ -22,6 +25,7 @@ public class PollService {
         return null;
     }
 
+    // Verwijdert alle polls uit de lijst
     public void clearPolls() {
         polls.clear();
     }
