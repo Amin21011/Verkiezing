3f22be2623468069a7c0331b27905232a0ebfafb
Morsal Asil <morsal.asil@hva.nl>
2025-10-09 19:56:22 +0200

    feat: made a start with candidate transformer

--- election-backend/src/main/java/nl/hva/election_backend/utils/xml/transformers/DutchCandidateTransformer.java
+++ election-backend/src/main/java/nl/hva/election_backend/utils/xml/transformers/DutchCandidateTransformer.java
@@ -1,14 +1,15 @@
 package nl.hva.election_backend.utils.xml.transformers;
 
+import nl.hva.election_backend.model.Candidate;
 import nl.hva.election_backend.model.Election;
 import nl.hva.election_backend.utils.xml.CandidateTransformer;
 
 import java.util.Map;
 
 /**
- * Just prints to content of electionData to the standard output.>br/>
- * <b>This class needs heavy modification!</b>
+ * Just prints to content of electionData to the standard output.
  */
+
 public class DutchCandidateTransformer implements CandidateTransformer {
     private final Election election;
 
@@ -23,6 +24,15 @@ public class DutchCandidateTransformer implements CandidateTransformer {
 
     @Override
     public void registerCandidate(Map<String, String> electionData) {
-        System.out.println("Registering candidate: " + electionData);
+        String candidateId = electionData.get("CandidateIdentifier");
+        String firstName = electionData.get("FirstName");
+        String lastName = electionData.get("LastName");
+        String partyId = electionData.get("AffiliationIdentifier");
+
+        if (candidateId != null && firstName != null && lastName != null) {
+            election.addCandidate(new Candidate(candidateId, candidateId, firstName, lastName, partyId));
+        } else {
+            System.out.println("Incomplete candidate data: " + electionData);
+        }
     }
 }
