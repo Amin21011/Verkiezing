fad1be0138806e7ebf707da9f70babf956727502
Morsal Asil <morsal.asil@hva.nl>
2025-10-10 14:09:33 +0200

    fix: small css change

--- election-frontend/src/App.vue
+++ election-frontend/src/App.vue
@@ -3,11 +3,9 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="min-h-screen w-screen bg-[#F8F7F3] flex flex-col items-center justify-start overflow-x-hidden overflow-y-auto">
-    <main class="w-full max-w-3xl p-6">
+
       <RouterView />
-    </main>
-  </div>
+
 </template>
 
 <style>
