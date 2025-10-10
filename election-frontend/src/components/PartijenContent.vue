aad626c5cecf4140a35fb1bdb784d89cb8d167f2
Morsal Asil <morsal.asil@hva.nl>
2025-10-10 02:08:17 +0200

    feat: implemented front end service in PartijenContent component of homepage

--- election-frontend/src/components/PartijenContent.vue
+++ election-frontend/src/components/PartijenContent.vue
@@ -1,7 +1,64 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useElectionResults } from "../helpers/ElectionResultsHelper";
+
+const { topParties, message, loadTopParties } = useElectionResults();
+
+onMounted(() => {
+  loadTopParties(3);
+});
+</script>
+
 <template>
-  <ul class="list-disc list-inside space-y-1 text-gray-700">
-    <li>🥇 Partij 1</li>
-    <li>🥈 Partij 2</li>
-    <li>🥉 Partij 3</li>
-  </ul>
+
+    <p v-if="message" class="message">{{ message }}</p>
+
+    <ul v-else class="party-list">
+      <li
+        v-for="(party, index) in topParties"
+        :key="party.id"
+        class="party-item"
+      >
+        <span class="rank">{{ index + 1 }}.</span>
+        <span class="name">{{ party.name }}</span>
+        <span class="votes">{{ party.voteCount.toLocaleString() }} stemmen</span>
+      </li>
+    </ul>
+
 </template>
+
+<style scoped>
+.party-list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.party-item {
+  display: flex;
+  justify-content: space-between;
+  padding: 4px 0;
+  border-bottom: 1px solid #ddd;
+  font-size: 0.95rem;
+}
+
+.party-item:last-child {
+  border-bottom: none;
+}
+
+.rank {
+  font-weight: bold;
+  width: 20px;
+}
+
+.name {
+  flex: 1;
+  text-align: left;
+  margin-left: 4px;
+}
+
+.votes {
+  font-size: 0.85rem;
+  color: #444;
+}
+</style>
