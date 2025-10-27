41e3f374be33603c20e280adf9ee642230df2348
Amin Mousoun <amin.mousoun@hva.nl>
2025-10-27 22:39:46 +0100

    feat: add CandidateCard.vue

---
+++ election-frontend/src/components/CandidateCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="border rounded-md p-4 shadow-sm hover:shadow-md transition">
+    <h3 class="font-bold text-lg">{{ candidate.getName() }}</h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  candidate: Object
+});
+</script>
