f5a636e0a3d2c8b2c5deed2d172b6d36f92f0765
Amin Mousoun <amin.mousoun@hva.nl>
2025-10-27 22:51:52 +0100

    feat: add CandidateService

---
+++ election-backend/src/main/java/nl/hva/election_backend/service/CandidateService.java
@@ -0,0 +1,26 @@
+package nl.hva.election_backend.service;
+
+import nl.hva.election_backend.model.Candidate;
+import nl.hva.election_backend.repository.CandidateRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CandidateService {
+
+    private final CandidateRepository candidateRepository;
+
+    public CandidateService(CandidateRepository candidateRepository) {
+        this.candidateRepository = candidateRepository;
+    }
+
+    public List<Candidate> getAllCandidates() {
+        return candidateRepository.findAll();
+    }
+
+    public List<Candidate> getCandidatesByParty(String partyId) {
+        return candidateRepository.findByPartyId(partyId);
+    }
+
+}
