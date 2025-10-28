bb3b86ccfbaf52e7e264301544b9e7a76e034ff2
Morsal Asil <morsal.asil@hva.nl>
2025-10-28 15:45:51 +0100

    feat: implemented H2 database for better testing

--- election-backend/src/main/java/nl/hva/election_backend/security/SecurityConfig.java
+++ election-backend/src/main/java/nl/hva/election_backend/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -27,14 +28,14 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 // Allow frontend to perform API calls
                 .cors(cors -> {})
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/electionresults/**", "/elections/**", "/api/polls/**").permitAll()
+                        .requestMatchers("/electionresults/**", "/elections/**", "/api/polls/**", "/h2/**").permitAll()
                         // Everything else requires a valid JWT
                         .anyRequest().authenticated()
                 )
-
-                // Our custom JWT filter validates tokens before Spring Security checks credentials
+                // custom JWT filter validates tokens before Spring Security checks credentials
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable);
