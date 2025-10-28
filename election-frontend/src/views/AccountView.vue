0e1f9fde49483dad1b6fafdebd2da6b368215fd9
Morsal Asil <morsal.asil@hva.nl>
2025-10-28 23:36:24 +0100

    feat: add account page components that display user profile when logged in

---
+++ election-frontend/src/views/AccountView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AccountDashboard from '@/components/AccountDashboard.vue'
+import NavBar from '@/components/NavBar.vue'
+import FooterComp from '@/components/FooterComp.vue'
+</script>
+
+<template>
+  <NavBar/>
+  <AccountDashboard/>
+  <FooterComp/>
+</template>
+
+<style>
+
+</style>
