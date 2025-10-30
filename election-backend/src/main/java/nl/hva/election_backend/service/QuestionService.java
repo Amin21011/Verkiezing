427b514f3d7726c7de8c4441cb9ab66370903308
Betül Aydin <betul.aydin@hva.nl>
2025-10-30 09:45:32 +0100

    renamed: question to questionservice

--- election-backend/src/main/java/nl/hva/election_backend/service/QuestionService.java
+++ election-backend/src/main/java/nl/hva/election_backend/service/QuestionService.java
@@ -1,13 +1,13 @@
-package nl.hva.election_backend.model;
+package nl.hva.election_backend.service;
 
 import java.util.Map;
 
-public class Question {
+public class QuestionService {
     private final String id;
     private final String text;
     private final Map<String, String> partyPositions; // partijnaam → standpunt
 
-    public Question(String id, String text, Map<String, String> partyPositions) {
+    public QuestionService(String id, String text, Map<String, String> partyPositions) {
         this.id = id;
         this.text = text;
         this.partyPositions = partyPositions;
