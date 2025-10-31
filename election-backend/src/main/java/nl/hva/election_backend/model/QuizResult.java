bfd7fc490df90064b10a537b96ede14e5a44bb30
Betül Aydin <betul.aydin@hva.nl>
2025-10-31 20:42:44 +0100

    feat: added quiz result model

--- election-backend/src/main/java/nl/hva/election_backend/model/QuizResult.java
+++ election-backend/src/main/java/nl/hva/election_backend/model/QuizResult.java
@@ -1,4 +1,21 @@
 package nl.hva.election_backend.model;
 
+import java.util.Map;
+
 public class QuizResult {
+    private final String bestMatchingParty;
+    private final Map<String, Integer> partyScores;
+
+    public QuizResult(String bestMatchingParty, Map<String, Integer> partyScores) {
+        this.bestMatchingParty = bestMatchingParty;
+        this.partyScores = partyScores;
+    }
+
+    public String getBestMatchingParty() {
+        return bestMatchingParty;
+    }
+
+    public Map<String, Integer> getPartyScores() {
+        return partyScores;
+    }
 }
