90a0b56ca884e47b93c9291ef11db40dd4b4a970
Betül Aydin <betul.aydin@hva.nl>
2025-10-31 22:47:11 +0100

    feat: added endpoint /quiz/result

--- election-backend/src/main/java/nl/hva/election_backend/controller/QuizController.java
+++ election-backend/src/main/java/nl/hva/election_backend/controller/QuizController.java
@@ -1,19 +1,27 @@
 package nl.hva.election_backend.controller;
 
 import nl.hva.election_backend.model.Quiz;
+import nl.hva.election_backend.model.QuizResult;
+import nl.hva.election_backend.service.QuizResultService;
 import nl.hva.election_backend.service.QuizService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/quiz")
 public class QuizController {
 
     private final QuizService quizService = new QuizService();
+    private final QuizResultService resultService = new QuizResultService();
 
     @GetMapping
     public Quiz getQuiz() {
         return quizService.getQuiz();
+    }
+
+    @PostMapping("/result")
+    public QuizResult getResult(@RequestBody Map<String, String> userAnswers) {
+        return resultService.calculateResult(userAnswers);
     }
 }
