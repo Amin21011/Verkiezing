f63327f590c0135eeeeefe3fecf4b676367a2d0a
Betül Aydin <betul.aydin@hva.nl>
2025-11-03 22:47:31 +0100

    feat: added QuestionService with endpoints for retrieving all questions or by ID

--- election-backend/src/main/java/nl/hva/election_backend/controller/QuizController.java
+++ election-backend/src/main/java/nl/hva/election_backend/controller/QuizController.java
@@ -1,11 +1,14 @@
 package nl.hva.election_backend.controller;
 
+import nl.hva.election_backend.model.Question;
 import nl.hva.election_backend.model.Quiz;
 import nl.hva.election_backend.model.QuizResult;
+import nl.hva.election_backend.service.QuestionService;
 import nl.hva.election_backend.service.QuizResultService;
 import nl.hva.election_backend.service.QuizService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -14,6 +17,8 @@ public class QuizController {
 
     private final QuizService quizService = new QuizService();
     private final QuizResultService resultService = new QuizResultService();
+    private final QuestionService questionService = new QuestionService();
+
 
     @GetMapping
     public Quiz getQuiz() {
@@ -26,5 +31,15 @@ public class QuizController {
             throw new IllegalArgumentException("Er zijn geen antwoorden ontvangen.");
         }
         return resultService.calculateResult(userAnswers);
+    }
+
+    @GetMapping("/questions")
+    public List<Question> getQuestions() {
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("/questions/{id}")
+    public Question getQuestionById(@PathVariable String id) {
+        return questionService.getQuestionById(id);
     }
 }
