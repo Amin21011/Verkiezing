93f3d12d85b962be3dd8c355bfcb8d9358cdd340
Ramzi Chejjar <ramzi.chejjar@hva.nl>
2025-11-04 14:53:11 +0100

    feat: Made a Service for the Forum that talks with the repository and model

---
+++ election-backend/src/main/java/nl/hva/election_backend/service/ForumService.java
@@ -0,0 +1,25 @@
+package nl.hva.election_backend.service;
+
+import nl.hva.election_backend.model.ForumPost;
+import nl.hva.election_backend.repository.ForumRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ForumService {
+
+    private final ForumRepository forumRepository;
+
+    public ForumService(ForumRepository forumRepository) {
+        this.forumRepository = forumRepository;
+    }
+
+    public List<ForumPost> getAllPosts() {
+        return forumRepository.findAllByOrderByPostedAtDesc();
+    }
+
+    public ForumPost addPost(ForumPost forumPost) {
+        return forumRepository.save(forumPost);
+    }
+}
