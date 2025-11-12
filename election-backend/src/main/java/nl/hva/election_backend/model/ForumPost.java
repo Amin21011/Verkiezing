27453b093c04f7ddff6753e517ee4a60d2feb026
Ramzi Chejjar <ramzi.chejjar@hva.nl>
2025-11-12 20:33:06 +0100

    feat: connected the forumPost to the User model so that the user gets linked to the question/statement they post

--- election-backend/src/main/java/nl/hva/election_backend/model/ForumPost.java
+++ election-backend/src/main/java/nl/hva/election_backend/model/ForumPost.java
@@ -13,27 +13,28 @@ public class ForumPost {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private String username;
     private String title;
     private String content;
     private LocalDateTime postedAt = LocalDateTime.now();
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
     public ForumPost() {}
 
-    public ForumPost(String username, String title, String content) {
-        this.username = username;
+    public ForumPost(String title, String content) {
         this.title = title;
         this.content = content;
         this.postedAt = LocalDateTime.now();
+
     }
 
     public long getId() {
         return id;
     }
 
-    public String getUsername() {
-        return username;
-    }
 
     public String getTitle() {
         return title;
@@ -45,14 +46,18 @@ public class ForumPost {
     public LocalDateTime getPostedAt() {
         return postedAt;
     }
+    public User getUser() { return user; }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
     public void setTitle(String title) {
         this.title = title;
     }
     public void setContent(String content) {
         this.content = content;
+    }
+    public void setPostedAt(LocalDateTime postedAt) {
+        this.postedAt = postedAt;
+    }
+    public void setUser(User user) {
+        this.user = user;
     }
 }
