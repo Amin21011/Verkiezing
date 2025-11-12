4a272148a2c0c3983f7f959d5e2d2d1ba28833ca
Morsal Asil <morsal.asil@hva.nl>
2025-11-12 19:08:55 +0100

    docs: add daily standup

---
+++ Algorithm/Datastructure/Morsal_Asil/DemoTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DemoTest {
+
+    private DutchPartyParser parser;
+
+    @BeforeEach
+
+    @Test
+
+}
