3fe12dd71447649cc0cb7645ca1ac2b3ac1833c4
Amin Mousoun <amin.mousoun@hva.nl>
2025-11-25 16:52:57 +0100

    feat: add the votes per user from localStorage

--- election-frontend/src/views/PollView.vue
+++ election-frontend/src/views/PollView.vue
@@ -15,27 +15,66 @@ const polls = ref<Poll[]>([]);
 const message = ref("");
 
 // Laad polls
+const userVotes = ref<Record<number, number>>({});
+// Load keuze uit localStorage
+function loadUserVotes() {
+  const saved = localStorage.getItem("userVotes");
+  userVotes.value = saved ? JSON.parse(saved) : {};
+}
+
+// Save keuze
+function saveUserVotes() {
+  localStorage.setItem("userVotes", JSON.stringify(userVotes.value));
+}
+
+// Polls laden
 async function loadPolls() {
   try {
     const res = await fetch(API_URL);
     polls.value = await res.json();
-    console.log("Polls geladen:", polls.value);
   } catch (error) {
     console.log("Fout bij het laden van polls:", error);
   }
 }
 
 async function vote(pollId: number, optionIndex: number) {
+  const previousVote = userVotes.value[pollId];
+
+  if (previousVote === optionIndex) {
+    message.value = "Je hebt al gestemd!";
+    setTimeout(() => (message.value = ""), 3000); // verdwijnt na 3 sec
+    return;
+  }
+
+  if (previousVote !== undefined) {
+    await fetch(`${API_URL}/${pollId}/reset/${previousVote}`, {
+      method: "PUT",
+    });
+  }
+
   await fetch(`${API_URL}/${pollId}/vote/${optionIndex}`, {
     method: "POST",
   });
 
+  userVotes.value[pollId] = optionIndex;
+  saveUserVotes();
+
   await loadPolls();
 }
 
-onMounted(loadPolls);
+
+
+onMounted(() => {
+  loadUserVotes();
+  loadPolls();
+});
 </script>
 
 <template>
-  <PollContent :polls="polls" :message="message" @vote="vote" />
+  <PollContent
+    :polls="polls"
+    :message="message"
+    :userVotes="userVotes"
+    @vote="vote"
+  />
 </template>
