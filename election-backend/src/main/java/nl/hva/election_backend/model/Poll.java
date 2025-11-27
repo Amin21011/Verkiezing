4d18ed48b977581c32f174d72eb8cdceb69f1f4d
Amin Mousoun <amin.mousoun@hva.nl>
2025-11-27 11:05:33 +0100

    feat: add an active state for the poll

--- election-backend/src/main/java/nl/hva/election_backend/model/Poll.java
+++ election-backend/src/main/java/nl/hva/election_backend/model/Poll.java
@@ -19,6 +19,7 @@ public class Poll {
     @ElementCollection
     private List<Integer> votes = new ArrayList<>();
 
+    private boolean active = false;
     public Poll() {}
 
     public Poll(String question, List<String> options) {
@@ -28,34 +29,17 @@ public class Poll {
         for (int i = 0; i < options.size(); i++) {
             this.votes.add(0);
         }
+        this.active = false;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    // Pollvraag teruggeven
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public List<String> getOptions() {
-        return options;
-    }
-
-    public void setOptions(List<String> options) {
-        this.options = options;
-    }
-
-    public List<Integer> getVotes() {
-        return votes;
-    }
-
-    public void setVotes(List<Integer> votes) {
-        this.votes = votes;
-    }
+    // Getters & setters
+    public Long getId() { return id; }
+    public String getQuestion() { return question; }
+    public void setQuestion(String question) { this.question = question; }
+    public List<String> getOptions() { return options; }
+    public void setOptions(List<String> options) { this.options = options; }
+    public List<Integer> getVotes() { return votes; }
+    public void setVotes(List<Integer> votes) { this.votes = votes; }
+    public boolean isActive() { return active; }
+    public void setActive(boolean active) { this.active = active; }
 }
