ef2b05cd6ac611231ce93db3fde8714d5d9905fe
Amin Mousoun <amin.mousoun@hva.nl>
2025-12-09 16:18:35 +0100

    feat: add the frontend comparecandidates

---
+++ election-frontend/src/components/CompareCandidates.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="max-w-md mx-auto bg-white/90 rounded-xl shadow-md p-6 space-y-4 backdrop-blur-sm">
+    <h1>Compare Candidates</h1>
+  </div>
+</template>
