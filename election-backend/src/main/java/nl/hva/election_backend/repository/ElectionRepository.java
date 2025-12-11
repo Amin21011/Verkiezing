642d2e587bf3bdb99e5a545eb20a43610854cd7c
Morsal Asil <morsal.asil@hva.nl>
2025-12-11 12:38:00 +0100

    fix(db): updated repositories for better parsing and import

--- election-backend/src/main/java/nl/hva/election_backend/repository/ElectionRepository.java
+++ election-backend/src/main/java/nl/hva/election_backend/repository/ElectionRepository.java
@@ -1,6 +1,9 @@
-//package nl.hva.election_backend.repository;
-//
-//import nl.hva.election_backend.model.Election;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ElectionRepository extends JpaRepository<Election, String> {}
+package nl.hva.election_backend.repository;
+
+import nl.hva.election_backend.model.Election;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ElectionRepository extends JpaRepository<Election, String> {
+}
