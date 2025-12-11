a5e119d8277a7e853829e215c0209c4db79ba4db
Morsal Asil <morsal.asil@hva.nl>
2025-12-11 12:37:52 +0100

    fix(db): updated repositories for better parsing and import

--- election-backend/src/main/java/nl/hva/election_backend/repository/PartyRepository.java
+++ election-backend/src/main/java/nl/hva/election_backend/repository/PartyRepository.java
@@ -1,31 +1,11 @@
 package nl.hva.election_backend.repository;
 import nl.hva.election_backend.model.Party;
-import nl.hva.election_backend.model.Party1;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.ArrayList;
+
 import java.util.List;
 
-
 @Repository
-public class PartyRepository {
-    private final List<Party> parties = new ArrayList<>();
-    private final List<Party1> partiesLong = new ArrayList<>();
-
-    public List<Party> getAll() {
-        return new ArrayList<>(parties);
-    }
-
-    public List<Party1> getAllParties() {
-        return new ArrayList<>(partiesLong);
-    }
-
-    public List<Party> findByNameContainingIgnoreCase(String name) {
-        List<Party> result = new ArrayList<>();
-        for (Party p : parties) {
-            if (p.getName().toLowerCase().contains(name.toLowerCase())) {
-                result.add(p);
-            }
-        }
-        return result;
-    }
+public interface PartyRepository extends JpaRepository<Party, String> {
+    List<Party> findByNameContainingIgnoreCase(String name);
 }
