265490d8a7deb05c1627a2ac9d35f76aeb59daa3
Morsal Asil <morsal.asil@hva.nl>
2025-12-11 13:36:43 +0100

    feat(parser): updated ElectionParserFactory and parsing flow

--- election-backend/src/main/java/nl/hva/election_backend/service/ElectionParserFactory.java
+++ election-backend/src/main/java/nl/hva/election_backend/service/ElectionParserFactory.java
@@ -1,21 +1,36 @@
 package nl.hva.election_backend.service;
-
 import nl.hva.election_backend.model.Election;
+import nl.hva.election_backend.repository.*;
 import nl.hva.election_backend.utils.xml.DutchElectionParser;
 import nl.hva.election_backend.utils.xml.transformers.*;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ElectionParserFactory {
+    private final ResultRepository resultRepository;
+    private final ElectionRepository electionRepository;
+    private final RegionRepository regionRepository;
+    private final CandidateRepository candidateRepository;
+    private final PartyRepository partyRepository;
+
+    public ElectionParserFactory(ResultRepository resultRepository,
+                                 ElectionRepository electionRepository,
+                                 RegionRepository regionRepository,
+                                 CandidateRepository candidateRepository, PartyRepository partyRepository) {
+        this.resultRepository = resultRepository;
+        this.electionRepository = electionRepository;
+        this.regionRepository = regionRepository;
+        this.candidateRepository = candidateRepository;
+        this.partyRepository = partyRepository;
+
+    }
 
     public DutchElectionParser createDutchParser(Election election) {
-        return new DutchElectionParser(
-                new DutchDefinitionTransformer(election),
-                new DutchCandidateTransformer(election),
-                new DutchResultTransformer(election),
-                new DutchNationalVotesTransformer(election),
-                new DutchConstituencyVotesTransformer(election),
-                new DutchMunicipalityVotesTransformer(election)
+        return new DutchElectionParser(election, electionRepository,
+                new DutchDefinitionTransformer(election, partyRepository, regionRepository),
+                new DutchCandidateTransformer(election, candidateRepository),
+                new DutchResultTransformer(election, resultRepository, candidateRepository, regionRepository, partyRepository),
+                new DutchVotesTransformer(election, resultRepository, regionRepository, partyRepository, candidateRepository)
         );
     }
 }
--- election-backend/src/main/java/nl/hva/election_backend/utils/xml/DutchElectionParser.java
+++ election-backend/src/main/java/nl/hva/election_backend/utils/xml/DutchElectionParser.java
@@ -1,65 +1,64 @@
 package nl.hva.election_backend.utils.xml;
-
+import nl.hva.election_backend.model.Election;
+import nl.hva.election_backend.repository.ElectionRepository;
 import nl.hva.election_backend.utils.PathUtils;
+import nl.hva.election_backend.utils.xml.transformers.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.stream.XMLStreamException;
+import org.xml.sax.helpers.DefaultHandler;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class DutchElectionParser {
     private static final Logger log = LoggerFactory.getLogger(DutchElectionParser.class);
-
+    private final Election election;
+    private final ElectionRepository electionRepository;
     private final DefinitionTransformer definitionTransformer;
     private final CandidateTransformer candidateTransformer;
-    private final VotesTransformer resultTransformer;
-    private final VotesTransformer nationalVotesTransformer;
-    private final VotesTransformer constituencyVotesTransformer;
-    private final VotesTransformer municipalityVotesTransformer;
+    private final DutchResultTransformer resultTransformer;
+    private final DutchVotesTransformer votesTransformer;
 
-    public DutchElectionParser(DefinitionTransformer definitionTransformer,
-                               CandidateTransformer candidateTransformer,
-                               VotesTransformer resultTransformer,
-                               VotesTransformer nationalVotesTransformer,
-                               VotesTransformer constituencyVotesTransformer,
-                               VotesTransformer municipalityVotesTransformer) {
+    public DutchElectionParser(
+            Election election,
+            ElectionRepository electionRepository,
+            DefinitionTransformer definitionTransformer,
+            CandidateTransformer candidateTransformer,
+            DutchResultTransformer resultTransformer,
+            DutchVotesTransformer votesTransformer
+    ) {
+        this.election = election;
+        this.electionRepository = electionRepository;
         this.definitionTransformer = definitionTransformer;
         this.candidateTransformer = candidateTransformer;
         this.resultTransformer = resultTransformer;
-        this.nationalVotesTransformer = nationalVotesTransformer;
-        this.constituencyVotesTransformer = constituencyVotesTransformer;
-        this.municipalityVotesTransformer = municipalityVotesTransformer;
+        this.votesTransformer = votesTransformer;
     }
 
-    public void parseResults(String electionId, String folderName)
-            throws IOException, XMLStreamException, ParserConfigurationException, SAXException {
-
-        if (folderName == null || folderName.isBlank()) {
-            throw new IllegalArgumentException("Folder name cannot be null or blank");
-        }
-
-        log.info("Loading election data from folder: {}", folderName);
-
-        parseFiles(folderName, "Verkiezingsdefinitie_" + electionId, new EMLHandler(definitionTransformer));
-        parseFiles(folderName, "Kandidatenlijsten_" + electionId, new EMLHandler(candidateTransformer));
-        parseFiles(folderName, "Resultaat_" + electionId, new EMLHandler(resultTransformer));
-        parseFiles(folderName, "Totaaltelling_" + electionId, new EMLHandler(nationalVotesTransformer));
-        parseFiles(folderName, "Telling_" + electionId + "_kieskring", new EMLHandler(constituencyVotesTransformer));
-        parseFiles(folderName, "Telling_" + electionId + "_gemeente", new EMLHandler(municipalityVotesTransformer));
-
-        log.info("Election {} parsed successfully: [Definition, Candidates, Results✔]", electionId);
+    public void parseResults(String electionId, String folderName) throws IOException, SAXException {
+        parseFiles(folderName, "Verkiezingsdefinitie_" + electionId,
+                () -> new EMLHandler(definitionTransformer));
+        parseFiles(folderName, "Kandidatenlijsten_" + electionId,
+                () -> new EMLHandler(candidateTransformer));
+        electionRepository.save(this.election);
+        Supplier<DefaultHandler> voteHandlerSupplier = () -> new EMLHandler(votesTransformer);
+        parseFiles(folderName, "Totaaltelling_" + electionId, voteHandlerSupplier);
+        parseFiles(folderName, "Telling_" + electionId + "_kieskring", voteHandlerSupplier);
+        parseFiles(folderName, "Telling_" + electionId + "_gemeente", voteHandlerSupplier);
+        parseFiles(folderName, "Resultaat_" + electionId,
+                () -> new EMLHandler(resultTransformer));
+        log.info("Election {} parsed successfully.", electionId);
     }
 
-    private void parseFiles(String folderName, String fileFilter, EMLHandler emlHandler)
-            throws IOException, ParserConfigurationException, SAXException {
-
+    private void parseFiles(String folderName, String fileFilter, Supplier<DefaultHandler> handlerSupplier)
+            throws IOException, SAXException {
         List<Path> files = PathUtils.findFilesToScan(folderName, fileFilter);
         files.sort(Comparator.comparing(Path::getFileName));
 
@@ -68,18 +67,23 @@ public class DutchElectionParser {
             return;
         }
 
-        for (Path electionFile : files) {
-            log.info("Processing file: {}", electionFile.getFileName());
-            try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(electionFile.toString()), 64 * 1024)) {
-                var factory = javax.xml.parsers.SAXParserFactory.newInstance();
-                factory.setNamespaceAware(true);
-                var parser = factory.newSAXParser();
-                emlHandler.setFileName(electionFile.toString());
-                parser.parse(bis, emlHandler);
-                log.debug("Processed file: {}", electionFile.getFileName());
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+
+        for (Path p : files) {
+            log.info("Processing file: {}", p.getFileName());
+            try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(p.toFile()), 64 * 1024)) {
+                SAXParser parser = factory.newSAXParser();
+                DefaultHandler handler = handlerSupplier.get();
+
+                if (handler instanceof EMLHandler) {
+                    ((EMLHandler) handler).setFileName(p.getFileName().toString());
+                }
+                parser.parse(bis, handler);
+
             } catch (Exception e) {
-                log.error("Error processing file {}: {}", electionFile.getFileName(), e.getMessage());
-                throw e;
+                log.error("Error processing file {}: {}", p.getFileName(), e.getMessage());
+                throw new SAXException("Failed to parse " + p.getFileName(), e);
             }
         }
     }
