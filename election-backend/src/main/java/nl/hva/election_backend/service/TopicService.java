4274fe777752362cdabc9eb769673f36bf18bb1e
Amin Mousoun <amin.mousoun@hva.nl>
2025-12-12 16:26:46 +0100

    feat: add TopicService with the methods for managing topics

---
+++ election-backend/src/main/java/nl/hva/election_backend/service/TopicService.java
@@ -0,0 +1,30 @@
+package nl.hva.election_backend.service;
+
+import nl.hva.election_backend.model.Topic;
+import nl.hva.election_backend.repository.TopicRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TopicService {
+
+    private final TopicRepository topicRepository;
+
+    public TopicService(TopicRepository topicRepository) {
+        this.topicRepository = topicRepository;
+    }
+
+    public Topic addTopic(Topic topic) {
+        return topicRepository.save(topic);
+    }
+
+    public List<Topic> getAllTopics() {
+        return topicRepository.findAll();
+    }
+
+    public Optional<Topic> getTopicById(Long id) {
+        return topicRepository.findById(id);
+    }
+}
