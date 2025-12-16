04dee36e8c736a10053a99d7886c45b4ba2a4695
Morsal Asil <morsal.asil@hva.nl>
2025-12-16 15:35:02 +0100

    feat(): implemented statistics and result endpoints

---
+++ election-backend/src/main/java/nl/hva/election_backend/api/StatisticsController.java
@@ -0,0 +1,23 @@
+package nl.hva.election_backend.api;
+
+import nl.hva.election_backend.dto.DailyFactResponse;
+import nl.hva.election_backend.service.DailyFactService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/statistics")
+public class StatisticsController {
+
+    private final DailyFactService dailyFactService;
+
+    public StatisticsController(DailyFactService dailyFactService) {
+        this.dailyFactService = dailyFactService;
+    }
+
+    @GetMapping("/daily-fact")
+    public List<DailyFactResponse> getDailyFacts() {
+        return dailyFactService.getDailyFacts();
+    }
+}
