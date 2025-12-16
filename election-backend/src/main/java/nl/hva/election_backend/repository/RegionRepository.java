481271c4d56fb59ec2f6490ca91d39aa2325bd0c
Morsal Asil <morsal.asil@hva.nl>
2025-12-16 15:00:19 +0100

    feat(): modified JPA repositories for election entities

--- election-backend/src/main/java/nl/hva/election_backend/repository/RegionRepository.java
+++ election-backend/src/main/java/nl/hva/election_backend/repository/RegionRepository.java
@@ -3,6 +3,8 @@ import nl.hva.election_backend.model.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RegionRepository extends JpaRepository<Region, String> {
 }
