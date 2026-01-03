138b17ea71be368a5d75d950c1fd2725e877b1f5
Amin Mousoun <amin.mousoun@hva.nl>
2026-01-03 20:26:46 +0100

    refactor: use TopicDTO in TopicController for creating topics

--- election-backend/src/main/java/nl/hva/election_backend/api/TopicController.java
+++ election-backend/src/main/java/nl/hva/election_backend/api/TopicController.java
@@ -2,6 +2,7 @@ package nl.hva.election_backend.api;
 
 import nl.hva.election_backend.model.Topic;
 import nl.hva.election_backend.service.TopicService;
+import nl.hva.election_backend.dto.model.TopicDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,7 +31,8 @@ public class TopicController {
     }
 
     @PostMapping
-    public ResponseEntity<Topic> addTopic(@RequestBody Topic topic) {
+    public ResponseEntity<Topic> addTopic(@RequestBody TopicDTO dto) {
+        Topic topic = new Topic(dto.getName());
         Topic saved = topicService.addTopic(topic);
         return ResponseEntity.ok(saved);
     }
