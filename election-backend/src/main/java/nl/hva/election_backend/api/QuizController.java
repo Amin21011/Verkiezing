3d096269f8bb6d646aba68bf281d7e6ee94101aa
Ramzi Chejjar <ramzi.chejjar@hva.nl>
2026-01-08 00:45:24 +0100

    fix: URL path fixe for the quiz

--- election-backend/src/main/java/nl/hva/election_backend/api/QuizController.java
+++ election-backend/src/main/java/nl/hva/election_backend/api/QuizController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/quiz")
+@RequestMapping("/api/quiz")
 public class QuizController {
 
     private final QuizService quizService;
