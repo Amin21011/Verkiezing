27762f747847853f361d1b5458f6bd5b2fcf70a3
Ramzi Chejjar <ramzi.chejjar@hva.nl>
2026-01-07 12:21:49 +0100

    fix: URL path fix

--- election-frontend/src/services/electionService.ts
+++ election-frontend/src/services/electionService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://oege.ie.hva.nl:9122/electionresults/'
+const API_BASE_URL = 'http://oege.ie.hva.nl:9122/api/'
 
 export const getTopParties = async (limit = 5) => {
   const res = await axios.get(`${API_BASE_URL}parties/top`, {
